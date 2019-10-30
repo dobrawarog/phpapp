87dd408e9433f90e45cf7d24d10982c032ca1c08
kl2ag1 <you@example.com>
2019-10-30 09:12:48 +0100

    aplikacja PHP

---
+++ db/baza.sql
@@ -0,0 +1,10 @@
+DROP TABLE IF EXISTS menu;
+CREATE TABLE menu (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  plik CHAR(20) NOT NULL,
+  tytul VARCHAR(30) NOT NULL,
+  pozycja INTEGER DEFAULT 0
+);
+INSERT INTO menu VALUES(NULL, 'witam', 'witamy', 1);
+INSERT INTO menu VALUES(NULL, 'formularz', 'formularz', 2);
+INSERT INTO menu VALUES(NULL, 'klasa', 'klasa', 3);
